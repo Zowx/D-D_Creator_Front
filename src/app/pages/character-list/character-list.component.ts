4bb68c917723079bbb856fbce7165ef002ec64fc
zowx <angles.coude.0f@icloud.com>
2025-04-15 15:59:36 +0200

    ✨ feat: implement character form component with validation and integration in character list page

--- src/app/pages/character-list/character-list.component.ts
+++ src/app/pages/character-list/character-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterFormComponent } from '../../components/character-form/character-form.component';
 
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
   styleUrls: ['./character-list.component.scss'],
+  standalone: true,
+  imports: [CommonModule, CharacterFormComponent],
 })
 export class CharacterListComponent {
   // Logic for the Character List page can be added here
+  showForm = true; // Variable pour contrôler l'affichage du formulaire
 }
