0a4343d806b48c5fda741682d87f4e04c5516995
zowx <angles.coude.0f@icloud.com>
2025-04-15 14:35:14 +0200

    🎉 reset angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,3 @@
 import { Routes } from '@angular/router';
-import {CreationPersoComponent } from './Pages/creation-perso/creation-perso.component';
-import {HomeComponent } from './Pages/home/home.component';
-import {PersonagesComponent } from './Pages/personages/personages.component';
 
-
-export const routes: Routes = [
-    {
-        path: "creation-perso",
-        component: CreationPersoComponent
-    },
-    {
-        path: "home",
-        component: HomeComponent
-    },
-    {
-        path: "personages",
-        component: PersonagesComponent
-    },
-    {
-        path: "**",
-        redirectTo: '/home'
-    }
-];
+export const routes: Routes = [];
