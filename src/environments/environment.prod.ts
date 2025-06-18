f665d4097b7b80afa694cadc90a8715606d982cb
zowx <angles.coude.0f@icloud.com>
2025-06-18 14:20:27 +0200

    ✨ feat: add environment configuration files for production and development

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: process.env['NG_APP_API_URL'],
+  version: '1.0.0',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: process.env['NG_APP_API_URL'],
+  version: '1.0.0',
+};
